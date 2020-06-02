a49ded2c1c5a69300f58b7b9ab00f7a0aa20c960
furenard <39519381+furenard@users.noreply.github.com>
2020-06-02 09:00:14 +0200

    Exo2+Exo3
    Exercice 2 et exercice 3

---
+++ Exercice2Form/Exercice2Form/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercice2Form
+{
+    public partial class Form1 : Form
+    {
+        String result;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 1;
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 2;
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 3;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 4;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 5;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 6;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 7;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 8;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textBox1.Text += 9;
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            String textePlus;
+            if (btn != null)
+            {
+                result += btn.Text;
+                textePlus = btn.Text;
+                textBox1.Text += textePlus;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            String texteMoins;
+            if (btn != null)
+            {
+                result += btn.Text;
+                texteMoins = btn.Text;
+                textBox1.Text += texteMoins;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            String texteMult;
+            if (btn != null)
+            {
+                result += btn.Text;
+                texteMult = btn.Text;
+                textBox1.Text += texteMult;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            String texteDiv;
+            if (btn != null)
+            {
+                result += btn.Text;
+                texteDiv = btn.Text;
+                textBox1.Text += texteDiv;
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            String texteBox;
+            String temp = ""; 
+            Char[] tableauCaractere = new char[20];
+            double[] tableauNombre = new double[20];
+            double resultat = 0;
+            int compteurCarac = 0;
+            int compteurNbre = 0;
+            if (btn != null)
+            {
+                texteBox = btn.Text;
+                textBox1.Text += texteBox;
+            }
+            foreach (char number in textBox1.Text)
+            {
+                switch (number)
+                {
+                case '+':
+                        tableauCaractere[compteurCarac] = '+';
+                        tableauNombre[compteurNbre] = Convert.ToDouble(temp);
+                        compteurNbre ++;
+                        compteurCarac++;
+                        temp = "";
+                        break;
+                case '-':
+                        tableauCaractere[compteurCarac] = '-';
+                        tableauNombre[compteurNbre] = Convert.ToDouble(temp);
+                        compteurNbre++;
+                        compteurCarac++;
+                        temp = "";
+                        break;
+                case '*':
+                        tableauCaractere[compteurCarac] = '*';
+                        tableauNombre[compteurNbre] = Convert.ToDouble(temp);
+                        compteurNbre++;
+                        compteurCarac++;
+                        temp = "";
+                        break;
+                case '/':
+                        tableauCaractere[compteurCarac] = '/';
+                        tableauNombre[compteurNbre] = Convert.ToDouble(temp);
+                        compteurNbre++;
+                        compteurCarac++;
+                        temp = "";
+                        break;
+                case '=':
+                        break;
+                    default:
+                        temp += number;
+                        break;
+
+                }
+
+            }
+
+            for (int i = 0; i < compteurNbre; i++ )
+            {
+                switch (tableauCaractere[i])
+                {
+                    case '+':
+                        resultat += (tableauNombre[i] + tableauNombre[i + 1]);
+                        break;
+                    case '-':
+                        resultat += (tableauNombre[i] - tableauNombre[i + 1]);
+                        break;
+                    case '/':
+                        resultat += (tableauNombre[i] / tableauNombre[i + 1]);
+                        break;
+                    case '*':
+                        resultat += (tableauNombre[i] * tableauNombre[i + 1]);
+                        break;
+                }
+            }
+            textBox2.Text = resultat+"";
+        }
+    }
+ }
+
---
+++ Exercice3Lapin/Exercice3Lapin/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Exercice3Lapin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(481, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 305);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(157, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(302, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(12, 158);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(157, 158);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(302, 158);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(12, 304);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(157, 304);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(302, 304);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(139, 140);
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 48);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Shuffle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 48);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(705, 431);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 48);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Quit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(2, -1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(451, 452);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PuzzleBox";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(469, -1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(319, 331);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Original";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(502, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 505);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
